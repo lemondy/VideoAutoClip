ad27b0559d870a5f86714e600d50025d05f5bb21
lemondy <lemondy9@gmail.com>
2023-12-25 22:07:30 +0800

    add new file

--- VideoAutoClip/AssemblyInfo.cs
+++ VideoAutoClip/AssemblyInfo.cs
@@ -4,6 +4,7 @@
 // All Rights Reserved.
 
 using System.Windows;
+using log4net;
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
@@ -13,3 +14,5 @@ using System.Windows;
 //(used if a resource is not found in the page,
 // app, or any theme specific resource dictionaries)
 )]
+
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config", ConfigFileExtension = "config", Watch = true)]
